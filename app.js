2d699bf66f3f09d1680733e428b350f05c7b1ca2
Roberto Ffrench-Davis <roberto_ffrench@hotmail.com>
2020-09-02 19:00:09 -0400

    Files added service working and db working

---
+++ app.js
@@ -0,0 +1,33 @@
+// Auth Microservice. 
+require('dotenv').config(); // .env files
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+const PORT = process.env.PORT || 3306
+
+// Database
+const sequelize = require('./util/database');
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+
+// Adding middleware (these are executed for every request)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Using the routes
+app.use('/api/v1', authRoutes);
+
+
+sequelize.sync()
+    .then(result => {
+        console.log(result);
+        app.listen(PORT);
+    })
+    .catch(err => {
+        console.log(err);
+    })
